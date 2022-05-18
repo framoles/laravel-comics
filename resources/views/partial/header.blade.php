fb4107c2c6016a7e8b5732ddf1e97e3b29925b1e
Francesco Moles <francesco.moles@outlook.it>
2022-05-18 17:42:12 +0200

    upload img + working on header

---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <img src="../images/dc-logo.png" alt="DC Logo" />
+    <ul>
+        <li>characters</li>
+        <li id="active">comics</li>
+        <li>movies</li>
+        <li>tv</li>
+        <li>games</li>
+        <li>collectibles</li>
+        <li>videos</li>
+        <li>fans</li>
+        <li>news</li>
+        <li>shop</li>
+    </ul>
+</div>
